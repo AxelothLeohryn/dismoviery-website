84b7739c17bf7896e1fbd470e621e3d0fe413887
AxelothLeohryn <alejandro97ss@gmail.com>
2023-10-28 20:12:30 +0200

    add basic search functionality

--- script.js
+++ script.js
@@ -24,15 +24,25 @@ toggleButton.addEventListener("click", (event) => {
     : "fa-solid fa-bars";
 });
 //#endregion
-
+//Main section is where cards will be displayed
+const mainSection = document.getElementById("main-section");
+//options needed for fetch requests:
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgwNDUyMzA5OTlmZjEwNWYwODc1NzE3MDdmYmZjNyIsInN1YiI6IjY1MmQ4NTM3MDI0ZWM4MDEzYzU4ZmNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IxnAafvpj2cajIWF6g3BipJBl3uzCk58CaOmgUUBuxY",
+  },
+};
 //#region Main Page
 //Display de Main Page
 
 async function mainPageLoad() {
-  const mainSection = document.getElementById("main-section");
   await printMostPopular(mainSection);
   await printNowPlaying(mainSection);
   await printUpcoming(mainSection);
+  //Event listener for clicks on cards
   mainSection.addEventListener("click", async (event) => {
     const movieCard = event.target.closest(".movie-card");
     if (movieCard) {
@@ -45,17 +55,7 @@ async function mainPageLoad() {
   });
   //   adjustFontSizeToText();
 }
-
 async function printMostPopular(mainSection) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgwNDUyMzA5OTlmZjEwNWYwODc1NzE3MDdmYmZjNyIsInN1YiI6IjY1MmQ4NTM3MDI0ZWM4MDEzYzU4ZmNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IxnAafvpj2cajIWF6g3BipJBl3uzCk58CaOmgUUBuxY",
-    },
-  };
-
   await fetch(
     "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
     options
@@ -75,17 +75,7 @@ async function printMostPopular(mainSection) {
     })
     .catch((err) => console.error(err));
 }
-
 async function printNowPlaying(mainSection) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgwNDUyMzA5OTlmZjEwNWYwODc1NzE3MDdmYmZjNyIsInN1YiI6IjY1MmQ4NTM3MDI0ZWM4MDEzYzU4ZmNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IxnAafvpj2cajIWF6g3BipJBl3uzCk58CaOmgUUBuxY",
-    },
-  };
-
   await fetch(
     "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
     options
@@ -106,15 +96,6 @@ async function printNowPlaying(mainSection) {
     .catch((err) => console.error(err));
 }
 async function printUpcoming(mainSection) {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgwNDUyMzA5OTlmZjEwNWYwODc1NzE3MDdmYmZjNyIsInN1YiI6IjY1MmQ4NTM3MDI0ZWM4MDEzYzU4ZmNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IxnAafvpj2cajIWF6g3BipJBl3uzCk58CaOmgUUBuxY",
-    },
-  };
-
   await fetch(
     "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
     options
@@ -142,28 +123,20 @@ async function fetchAndDisplayMovieDetails(id) {
   movieDetails.style.display = "flex";
 
   let movieTrailerKey = "";
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTgwNDUyMzA5OTlmZjEwNWYwODc1NzE3MDdmYmZjNyIsInN1YiI6IjY1MmQ4NTM3MDI0ZWM4MDEzYzU4ZmNjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IxnAafvpj2cajIWF6g3BipJBl3uzCk58CaOmgUUBuxY",
-    },
-  };
   await fetch(
     `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
     options
   )
     .then((response) => response.json())
     .then((response) => {
-        //Get youtube trailer id key
+      //Get youtube trailer id key
       console.log(response.results.reverse());
-      response.results.forEach(video => {
-        if (video.type == 'Trailer') {
-            movieTrailerKey = video.key;
+      response.results.forEach((video) => {
+        if (video.type == "Trailer") {
+          movieTrailerKey = video.key;
         }
-      })
-    //   movieTrailerKey = response.results.reverse()[0].key;  NOT ALWAYS TRAILER
+      });
+      //   movieTrailerKey = response.results.reverse()[0].key;  NOT ALWAYS TRAILER
     })
     .catch((err) => console.error(err));
 
@@ -175,8 +148,10 @@ async function fetchAndDisplayMovieDetails(id) {
     .then((response) => {
       console.log(response);
       //Get movie genres
-      let movieGenres = '';
-      response.genres.forEach(genre => movieGenres += `<div class='genre-tag'>${genre.name}</div>`);
+      let movieGenres = "";
+      response.genres.forEach(
+        (genre) => (movieGenres += `<div class='genre-tag'>${genre.name}</div>`)
+      );
       console.log(movieGenres);
 
       movieDetails.innerHTML = `
@@ -214,8 +189,37 @@ async function fetchAndDisplayMovieDetails(id) {
       movieDetails.innerHTML = ""; //fix youtube video playing in background
     });
 }
-
 mainPageLoad();
+
+async function searchMovies(search) {
+  mainSection.innerHTML = "<h1>Results</h1>"; //Prepopulate with "Results:" and "Sort By:" sections
+  await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response);
+        let movieCardContainerHTML = `<section class="movie-card-container">`;
+        response.results.forEach(movie => {
+            movieCardContainerHTML += `<section class="movie-card" data-movie-id="${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Poster Image">
+            <h3 class="dynamic-font-size">${movie.title}</h3>
+        </section>`;
+    })
+    movieCardContainerHTML += `</section>`;
+    mainSection.innerHTML += movieCardContainerHTML;
+    })
+    .catch((err) => console.error(err));
+}
+//Event listener for Search Button
+document.getElementById("search-button").addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event.target.form[0].value);
+  if (event.target.form[0].value) {
+    searchMovies(event.target.form[0].value);
+  } else console.log("Please search something");
+});
 
 //#endregion
 
